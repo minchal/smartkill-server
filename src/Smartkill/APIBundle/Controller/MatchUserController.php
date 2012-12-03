bacaab1ac2784a430c8fc6fa4719307a3b0fda95
Andrzej Domanski <a.domanski@si2.pl>
2012-12-04 00:45:23 +0100

    Pozycje graczy - poprawki

--- src/Smartkill/APIBundle/Controller/MatchUserController.php
+++ src/Smartkill/APIBundle/Controller/MatchUserController.php
@@ -4,30 +4,65 @@ namespace Smartkill\APIBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Doctrine\ORM\NoResultException;
 
 class MatchUserController extends Controller {
 	
     public function positionAction() {
 		$this->checkSession();
-		
 		$request = $this -> getRequest();
+		
+		$user  = $request->request->get('user');
+		$match = $request->request->get('match');
+		$lat   = $request->request->get('lat');
+		$lng   = $request->request->get('lng');
+		
+		// aktualizacja pozycji użytkownika
+		if (!$this->updatePositionAction($user, $match, $lat, $lng)) {
+			return $this -> jsonResponse(array('msg'=>'User invalid'),'error');
+		}
+		
 		$repository = $this->getDoctrine()->getRepository('SmartkillWebBundle:MatchUser');
 		$query = $repository->createQueryBuilder('mu')
-			->where('mu.user = :user')
-			->andWhere('mu.match = :match')
-			->setParameters(array(
-		    	'user'  => $request->request->get('user_id'),
-		    	'match' => $request->request->get('match_id'),
-			))
+			->where('mu.match = :match')
+			->setParameter('match', $match)
+			->orderBy('mu.user', 'ASC')
 			->getQuery();
-		$position = $query->getSingleResult();
+		$positions = $query->getResult();
 		
-		if (!$position) {
-			return $this -> jsonResponse(array('msg'=>'Position undefinied'),'error');
+		if (!$positions) {
+			return $this -> jsonResponse(array('msg'=>'Match invalid'),'error');
 		}
 		
 		$serializer = $this->get('serializer');
-		return $this -> jsonResponse($serializer->serialize(array('status' => 'success', 'position'=>$position), 'json'));
+		return $this -> jsonResponse($serializer->serialize(array('status' => 'success', 'positions'=>$positions), 'json'));
+    }
+    
+    public function updatePositionAction($user, $match, $lat, $lng) {
+    	$em = $this->getDoctrine()->getManager();
+    	
+    	try {
+	    	$query = $em->getRepository('SmartkillWebBundle:MatchUser')->createQueryBuilder('mu')
+		    	->where('mu.match = :match')
+		    	->andWhere('mu.user = :user')
+		    	->setParameters(array(
+		    		'match' => $match,
+		    		'user'  => $user
+		    	  ))
+		    	->getQuery();
+	    	$user = $query->getSingleResult();
+    	} catch (NoResultException $e) {
+    		return false;
+    	}
+    	
+    	$user->setLat($lat);
+    	$user->setLng($lng);
+    	$user->setUpdatedAt(new \DateTime());
+    	
+    	$em->persist($user);
+    	$em->flush();
+    	
+    	return true;
     }
 	
 	private function checkSession() {
